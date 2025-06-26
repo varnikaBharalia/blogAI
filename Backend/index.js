5a91795fc50645ba063e44bc7a1102fa2e897ca4
varnikaBharalia <vbharalia11@gmail.com>
2025-06-26 20:09:25 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -18,15 +18,15 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: "https://blogify-frontend-lyart.vercel.app",
-  credentials: true,
-}));
-
 // app.use(cors({
-//   origin: "http://localhost:5173", 
+//   origin: "https://blogify-frontend-lyart.vercel.app",
 //   credentials: true,
 // }));
+
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,
+}));
 
 app.use(express.json());
 
