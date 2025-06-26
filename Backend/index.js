0b9b98a28de6f10cfaf56e0f0f4475eb70d15081
Mohit <hllomohit722@gmail.com>
2025-06-26 10:07:12 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "https://blogify-frontend-lyart.vercel.app/",
+  origin: "https://blogify-frontend-lyart.vercel.app",
   credentials: true,
 }));
 
